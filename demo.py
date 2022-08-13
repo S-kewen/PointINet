da6729ee02275372ce992fec8b825ace84a2f569
S-kewen <61229819+S-kewen@users.noreply.github.com>
2022-08-14 01:20:17 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -10,28 +10,61 @@ import argparse
 from tqdm import tqdm
 import os
 
+import trimesh
+from plyfile import PlyData
+import pandas as pd
+import open3d as o3d
+
+# envpath = '/home/skewen/anaconda3/envs/pointinet/lib/python3.6/site-packages/cv2/qt/plugins/platforms'
+# os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = envpath
+
+def ply2bin(input_path, output_path):
+    data = PlyData.read(input_path).elements[0].data  # read data
+    data_pd = pd.DataFrame(data)  # convert to DataFrame
+    # initialize array to store data
+    data_np = np.zeros(data_pd.shape, dtype=np.float)
+    property_names = data[0].dtype.names  # read names of properties
+    for i, name in enumerate(
+            property_names):  # read data by property
+        data_np[:, i] = data_pd[name]
+    data_np.astype(np.float32).tofile(output_path)
+
+
+def bin2ply(input_path, output_path):
+    xyzr = np.fromfile(input_path, dtype=np.float32).reshape(-1, 4)
+    xyz0 = xyzr[:, :3]
+    src_keypts = o3d.geometry.PointCloud()
+    src_keypts.points = o3d.utility.Vector3dVector(xyz0)
+    print(src_keypts)
+    o3d.io.write_point_cloud(output_path, src_keypts)
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description='demo')
     parser.add_argument('--gpu', type=str, default='0')
     parser.add_argument('--npoints', type=int, default=32768)
-    parser.add_argument('--pretrain_model', type=str, default='./pretrain_model/interp_kitti.pth')
-    parser.add_argument('--pretrain_flow_model', type=str, default='./pretrain_model/flownet3d_kitti_odometry_maxbias1.pth')
+    parser.add_argument('--pretrain_model', type=str,
+                        default='./pretrain_model/interp_kitti.pth')
+    parser.add_argument('--pretrain_flow_model', type=str,
+                        default='./pretrain_model/flownet3d_kitti_odometry_maxbias1.pth')
     parser.add_argument('--is_save', type=int, default=1)
     parser.add_argument('--visualize', type=int, default=1)
 
     return parser.parse_args()
 
+
 def get_lidar(fn, npoints):
-    points = np.fromfile(fn, dtype=np.float32, count=-1).reshape([-1,4])
+    points = np.fromfile(fn, dtype=np.float32, count=-1).reshape([-1, 4])
     raw_num = points.shape[0]
     if raw_num >= npoints:
         sample_idx = np.random.choice(raw_num, npoints, replace=False)
     else:
-        sample_idx = np.concatenate((np.arange(raw_num), np.random.choice(raw_num, npoints - raw_num, replace=True)), axis=-1)
-    
+        sample_idx = np.concatenate((np.arange(raw_num), np.random.choice(
+            raw_num, npoints - raw_num, replace=True)), axis=-1)
+
     pc = points[sample_idx, :]
     pc = torch.from_numpy(pc).t()
-    color = np.zeros([npoints,3]).astype('float32')
+    color = np.zeros([npoints, 3]).astype('float32')
     color = torch.from_numpy(color).t()
 
     pc = pc.unsqueeze(0).cuda()
@@ -39,20 +72,23 @@ def get_lidar(fn, npoints):
 
     return pc, color
 
+
 def demo(args):
     os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
 
-    fn1 = './data/demo_data/original/000000.bin'
-    fn2 = './data/demo_data/original/000001.bin'
+    fn1 = './data/demo_data/original/1.bin'  # frist frame
+    fn2 = './data/demo_data/original/2.bin'  # last frame
 
     net = PointINet()
-    net.load_state_dict(torch.load(args.pretrain_model))
-    net.flow.load_state_dict(torch.load(args.pretrain_flow_model))
+    net.load_state_dict(torch.load(args.pretrain_model))  # loading model
+    net.flow.load_state_dict(torch.load(
+        args.pretrain_flow_model))  # loading model
     net.eval()
     net.cuda()
 
-    interp_scale = 5
-    t_array = np.arange(1.0/interp_scale, 1.0, 1.0/interp_scale, dtype=np.float32)
+    interp_scale = 5  # interp how many frame
+    t_array = np.arange(1.0/interp_scale, 1.0, 1.0 /
+                        interp_scale, dtype=np.float32)
 
     with torch.no_grad():
         pc1, color1 = get_lidar(fn1, args.npoints)
@@ -65,27 +101,34 @@ def demo(args):
 
             pred_mid_pc = net(pc1, pc2, color1, color2, t)
 
-            ini_pc = pc1.squeeze(0).permute(1,0).cpu().numpy()
-            end_pc = pc2.squeeze(0).permute(1,0).cpu().numpy()
+            ini_pc = pc1.squeeze(0).permute(1, 0).cpu().numpy()
+            end_pc = pc2.squeeze(0).permute(1, 0).cpu().numpy()
 
-            
-            pred_mid_pc = pred_mid_pc.squeeze(0).permute(1,0).cpu().numpy()
+            pred_mid_pc = pred_mid_pc.squeeze(0).permute(1, 0).cpu().numpy()
 
             if args.visualize == 1:
-                fig = mlab.figure(figure=None, bgcolor=(1,1,1), fgcolor=(0,0,0), engine=None, size=(1600, 1000))
-                mlab.points3d(ini_pc[:,0],ini_pc[:,1],ini_pc[:,2],color=(0,0,1),scale_factor=0.2,figure=fig, mode='sphere')
-                mlab.points3d(end_pc[:,0],end_pc[:,1],end_pc[:,2],color=(0,1,0),scale_factor=0.2,figure=fig, mode='sphere')
-                mlab.points3d(pred_mid_pc[:,0],pred_mid_pc[:,1],pred_mid_pc[:,2],color=(1,0,0),scale_factor=0.2,figure=fig, mode='sphere')
+                fig = mlab.figure(figure=None, bgcolor=(1, 1, 1), fgcolor=(
+                    0, 0, 0), engine=None, size=(1600, 1000))
+                mlab.points3d(ini_pc[:, 0], ini_pc[:, 1], ini_pc[:, 2], color=(
+                    0, 0, 1), scale_factor=0.2, figure=fig, mode='sphere')
+                mlab.points3d(end_pc[:, 0], end_pc[:, 1], end_pc[:, 2], color=(
+                    0, 1, 0), scale_factor=0.2, figure=fig, mode='sphere')
+                mlab.points3d(pred_mid_pc[:, 0], pred_mid_pc[:, 1], pred_mid_pc[:, 2], color=(
+                    1, 0, 0), scale_factor=0.2, figure=fig, mode='sphere')
                 mlab.show()
-            
+
             if args.is_save == 1:
                 save_dir = './data/demo_data/interpolated'
                 if not os.path.exists(save_dir):
                     os.makedirs(save_dir)
-                save_name = os.path.join(save_dir, str(t.squeeze().cpu().numpy())+'.bin')
+                save_name = os.path.join(save_dir, str(
+                    t.squeeze().cpu().numpy())+'.bin')
                 pred_mid_pc.tofile(save_name)
                 print("save interpolated point clouds to:", save_name)
+
 
 if __name__ == '__main__':
     args = parse_args()
     demo(args)
+    # bin2ply('./data/demo_data/original/000000.bin','./data/demo_data/original/000000.ply')
+    # ply2bin('/home/skewen/UnrealEngine_4.26/Engine/Binaries/Linux/carla/PythonAPI/examples/output/0/10252.ply','./data/demo_data/original/3.bin')
